589fd7bb60f18e36c89be999941487b69c93e9d1
yungchu <linyungchu@gmail.com>
2023-10-16 01:09:56 +0800

    add nav and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="App">
-          <h1>My React App</h1>
+    <div className="App"> 
+          <Navbar />
+          <h1 className="mt-3 vh-100">My React App</h1>
+          <Footer />
     </div>
   );
 }
