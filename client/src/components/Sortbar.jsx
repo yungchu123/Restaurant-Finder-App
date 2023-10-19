53a244503c8f9ff61b61d531d5cb1ac5b6c0d9db
yungchu <linyungchu@gmail.com>
2023-10-19 17:15:39 +0800

    add sort bar

--- client/src/components/Sortbar.jsx
+++ client/src/components/Sortbar.jsx
@@ -1,18 +1,34 @@
+import { useState } from "react"
+
 const Sortbar = () => {
-  return (
-    <>
-        <div class="btn-group">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <li><a class="dropdown-item" href="/#">Menu item</a></li>
-                <li><a class="dropdown-item" href="/#">Menu item</a></li>
-                <li><a class="dropdown-item" href="/#">Menu item</a></li>
-            </ul>
-        </div>
-    </>
-  )
+    const [sortValue, setSortValue] = useState('Relevance');
+    
+    const sortValueArray = [
+        "Relevance",
+        "Distance",
+        "Rating: High to Low",
+        "Price: Low to High"
+    ]
+
+    const handleOnClick = (val) => {
+        setSortValue(val)
+    }
+
+    return (
+        <>
+            <div class="btn-group me-3">
+                <button class="btn btn-secondary dropdown-toggle"  type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span className="me-2">Sort by</span>
+                    <strong className="me-2">{sortValue}</strong>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    {sortValueArray.map(value => (
+                        <li onClick={() => handleOnClick(value)} className="dropdown-item">{value}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default Sortbar
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import '@popperjs/core'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css'
 import App from './App';
