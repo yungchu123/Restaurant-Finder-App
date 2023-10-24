6df879536b29e800e7ea3455a38dbd1cb4680130
yauuuuuu <ngshangyau@gmail.com>
2023-10-25 05:08:09 +0800

    Edited calling Google API in test.js for 5 separate coordinates

--- scripts/test.js
+++ scripts/test.js
@@ -10,13 +10,21 @@ const RawApiResponseSchema = new mongoose.Schema({}, { strict: false });
 const ApiResponse = mongoose.model('ApiResponse', RawApiResponseSchema);
 
 const GOOGLE_API_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
-const SINGAPORE_LOCATION = '1.3521,103.8198'
+//const SINGAPORE_LOCATION = '1.3521,103.8198'
 
-async function fetchRestaurants(nextPageToken) {
+const SINGAPORE_LOCATION_LIST = [
+    '1.345653,103.736804',   // Jurong East
+    '1.289307,103.824438',   // Tiong Bahru
+    '1.426941,103.827871',   // Yishun
+    '1.351776,103.924688',   // Paya Lebar
+    '1.333879,103.843559'    // Toa Payoh
+  ];
+
+async function fetchRestaurants(location, nextPageToken) {
     try {
         const response = await axios.get(GOOGLE_API_ENDPOINT, {
             params: {
-                location: SINGAPORE_LOCATION,
+                location: location,
                 radius: 50000, 
                 type: 'restaurant',
                 key: process.env.GOOGLE_API_KEY,
@@ -35,18 +43,24 @@ async function fetchRestaurants(nextPageToken) {
 
         if (response.data.next_page_token) {
             setTimeout(() => {
-                fetchRestaurants(response.data.next_page_token);
+                fetchRestaurants(location, response.data.next_page_token);
             }, 5000); 
         } else {
-            console.log('Fetching completed');
+            console.log(`Fetching completed for location: ${location}`);
             mongoose.connection.close();
         }
 
     } catch (error) {
-        console.error('Error fetching or saving restaurants:', error.message);
+        console.error(`Error fetching or saving restaurants for location ${location}:`, error.message);
         mongoose.connection.close();
     }
 }
 
 
-fetchRestaurants();
+async function fetchRestaurantsForLocations() {
+    for (const location of SINGAPORE_LOCATION_LIST) {
+      await fetchRestaurants(location);
+    }
+  }
+  
+fetchRestaurantsForLocations();
--- server/controllers/restaurantsController.js
+++ server/controllers/restaurantsController.js
@@ -1,25 +1,20 @@
-const Restaurant = require("../models/restaurantModel")
+const mongoose = require("mongoose");
 const asyncHandler = require('express-async-handler')
 const CustomError = require('../utils/customError')
+
+
+const ApiResponse = mongoose.model('ApiResponse', mongoose.Schema({}, { strict: false }));
 
 // @desc Get all restaurants in database
 // @route GET /restaurants
 // @access Public
 const getAllRestaurants = asyncHandler(async (req, res) => {
-    const restaurants = await Restaurant.find().lean()
+    const restaurants = await ApiResponse.find().lean()
     if (!restaurants?.length) {
         throw new CustomError(400, 'No Restaurants found')
     }
     res.json(restaurants)
 })
-
-constCreateNewRestaurant = asyncHandler(async (req, res) => { 
-
-    
-
-
-})
-
 
 
 module.exports = {
