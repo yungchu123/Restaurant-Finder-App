33039d428bc2964eb10bc4a8095f84a2aa6b0392
limshanshan <143702599+limshanshan@users.noreply.github.com>
2023-10-27 23:21:53 +0800

    Delete sc2006 directory

--- sc2006/venv/user_profiles/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class UserProfilesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_profiles'
-
-    def ready(self):
-        from . import signals  # Import your signal handling code here
