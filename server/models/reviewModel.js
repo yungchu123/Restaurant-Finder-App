eddbd5d099f41c5fee1e9eb36b92a816835e4a51
haofah14 <haofah14@gmail.com>
2023-10-30 18:09:53 +0800

    restaurant & review api updates

--- server/models/reviewModel.js
+++ server/models/reviewModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
     restaurantId: String,
+    restaurantName: String,
     author_name: String,
     rating: Number,
     text: String,
