966aaedadc6f6f756c0f2c47cc6ae601b373e4f9
limshanshan <143702599+limshanshan@users.noreply.github.com>
2023-11-11 04:58:18 +0800

    updated server side code for tables

--- sc2006/server/routes/restaurantRoutes.js
+++ sc2006/server/routes/restaurantRoutes.js
@@ -7,6 +7,10 @@ router
   .get(restaurantController.getAllRestaurants)
 
 router
+  .route("/search")
+  .get(restaurantController.getAllRestaurants)
+
+router
   .route("/register")
   .post(restaurantController.createNewRestaurant);
 
@@ -18,11 +22,24 @@ router
   .route("/:restaurantId")
   .get(restaurantController.getRestaurant);
 
-router.post("/:restaurantId/tables/reserve", restaurantController.reserveTable);
-router.post("/:restaurantId/tables/unreserve", restaurantController.unreserveTable);
+router
+  .route("/:restaurantId/tables/reserve")
+  .post(restaurantController.reserveTable);
+
+router
+  .route("/:restaurantId/tables/unreserve")
+  .post(restaurantController.unreserveTable);
 
 router
   .route("/:restaurantId/tables")
   .get(restaurantController.getTables);
+
+router
+  .route("/:restaurantId/tables")
+  .get(restaurantController.getTables);
+
+router
+  .route("/:restaurantId/reservations/accept")
+  .post(restaurantController.acceptReservation);
 
 module.exports = router;
